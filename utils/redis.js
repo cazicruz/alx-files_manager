890db1dcd2991351dbd9369181f004a4e24d53f2
david onwuli <dcazicruz@icloud.com>
2023-06-23 00:21:41 +0100

    file manager task

---
+++ utils/redis.js
@@ -0,0 +1,42 @@
+const redis = require('redis');
+
+class RedisClient{
+        constructor(){
+                this.client = redis.createClient();
+                this.client.on('error',(error)=>{
+                        console.error(error)
+                });
+        }
+
+        function isAlive(){
+                this.client.ping((error, reply)=>{
+                        if (error){
+                                return false;
+                        }
+                        else {
+                                return true;
+                        }
+                });
+        }
+
+        async get(value){
+                await this.client.get(value, (error, result)=>{
+                        if (error){
+                                console.log(error);
+                        } else{
+                                return result;
+                        }
+                })
+        };
+
+        async set(key, value, duration){
+                await this.client.set(key, value, EX, x);
+        };
+
+        async del(key){
+                await this.client.del(key)
+        }
+}
+
+redisClient = new RedisClient();
+module.exports = redisClient
